2e1c9415ab0fc5c0733bb0268fcc13f6ffe8be97
Mezbah <mezbah488@gmail.com>
2026-04-11 22:40:42 +0600

    update

--- lua/inkscape-figures/init.lua
+++ lua/inkscape-figures/init.lua
@@ -130,7 +130,8 @@ function M.setup(opts)
 		group = augroup,
 		once = true,
 		callback = function()
-			os.execute("taskkill /f /im python.exe >nul 2>&1")
+			os.execute('taskkill /fi "WINDOWTITLE eq Inkscape-Opener" /f >nul 2>&1')
+			os.execute('taskkill /fi "WINDOWTITLE eq SVG-Exporter" /f >nul 2>&1')
 		end,
 	})
 end
